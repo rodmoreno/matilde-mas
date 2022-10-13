27776c68a3bd92aae0d0dfbe5584c46c5a75fee2
Rodrigo Moreno <moreno.rdr@gmail.com>
2022-10-12 22:37:57 -0500

    featue(app): add auth0 and react-router config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
+import Auth0ProviderWithHistory from './components/auth0'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
+  </Router>
 )
